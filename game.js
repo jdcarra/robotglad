e9da07a065cd4140c3e0b95a1475656e158b3f96
mona Lisa <jonas.d.carrasco@outlook>
2022-05-15 13:31:22 -0500

    2nd update

---
+++ game.js
@@ -0,0 +1,5 @@
+window.alert("This is an alert! JavaScript is running!");
+// This creates a function named "fight" 
+function fight (The fight has begun !) {
+    window.alert("the fight has begun!");
+}
